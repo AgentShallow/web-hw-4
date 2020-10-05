503d0a8d96f8f0be9927d0ea627fc8b2af7fe91f
AgentShallow <70897680+AgentShallow@users.noreply.github.com>
2020-10-05 22:54:59 +0300

    Done

---
+++ Script.js
@@ -0,0 +1,12 @@
+let counter = 100;
+if (counter >= 100){
+    console.log("A")
+}else if (counter >= 80){
+    console.log("B")
+}else if (counter >= 70){
+    console.log("C")
+}else if  (counter >= 60){
+    console.log("D")
+}else{
+    console.log('F')
+}
